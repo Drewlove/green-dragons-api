763b149d80f949de9eac4f171a085867ff5c3738
Drew Love <davidandrewlove84@gmail.com>
2020-04-17 11:49:40 -0400

    remove hair_color from table: student

--- seeds/001.seed.student.sql
+++ seeds/001.seed.student.sql
@@ -1,10 +1,10 @@
 TRUNCATE TABLE student RESTART IDENTITY CASCADE;
 
-INSERT INTO student(first_name, last_name, birth_date, hair_color)
+INSERT INTO student(first_name, last_name, birth_date)
 VALUES
-('Mike', 'Cermak', '1980-09-01', 'black'),
-('Ale', 'Cabrera-Mondragon', '1994-06-01', 'auburn'),
-('Drew', 'Love', '1984-09-11', 'brown');
+('Mike', 'Cermak', '1980-09-01'),
+('Ale', 'Cabrera-Mondragon', '1994-06-01'),
+('Drew', 'Love', '1984-09-11');
 
 
 -- SEED SCRIPT
