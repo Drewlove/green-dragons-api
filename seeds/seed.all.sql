b75e398a31e182c3ac48fec07a61bd25d0410fb4
Drew Love <davidandrewlove84@gmail.com>
2020-04-29 17:09:19 -0400

    remove boilerplate migrations and seeds, add challenge migration and seed

--- seeds/seed.all.sql
+++ seeds/seed.all.sql
@@ -1,7 +1,7 @@
 BEGIN;
-\i /Users/Drew/Projects/green-dragons-api/seeds/001.seed.green_dragons_user.sql
-\i /Users/Drew/Projects/green-dragons-api/seeds/002.seed.user.sql
+\i /Users/Drew/Projects/green-dragons-api/seeds/001.seed.student.sql
+\i /Users/Drew/Projects/green-dragons-api/seeds/002.seed.challenge.sql
 
 COMMIT;
 
--- SEED ALL, copy and paste: psql -f ~/Projects/boilerplate-api/seeds/seed.all.sql boilerplate;
+-- SEED ALL, copy and paste: psql -f ~/Projects/green-dragons-api/seeds/seed.all.sql green_dragons;
