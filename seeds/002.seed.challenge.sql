f3a70030c5b1bf5838e22d0df6ab0f6f38ab8cf1
Drew Love <davidandrewlove84@gmail.com>
2020-05-18 17:20:39 -0400

    removed challenge_best_record column from challenge table

--- seeds/002.seed.challenge.sql
+++ seeds/002.seed.challenge.sql
@@ -1,34 +1,30 @@
 TRUNCATE TABLE challenge RESTART IDENTITY CASCADE;
 
 INSERT INTO challenge
-(challenge_name, challenge_description, challenge_best_record , units)
+(challenge_name, challenge_description, units)
 VALUES
 
 (
 'Jump Rope Challenge',
 'Jump rope as many times as you can in 1 minute.',
-'highest',
 'jumps'
 ),
 
 (
   'Fitness Boxing',
   'Hit as many targets as possible in 2 minutes.',
-  'highest',
   'seconds'
 ),
 
 (
   'Archery Accuracy',
   'Score as many points as you can in 5 shots with the bow and arrow',
-  'highest',
   'targets'
 ),
 
 (
   '1/2 Mile Run',
   'Run a 1/2 mile as fast as possible.',
-  'lowest',
   'seconds');
 
 -- SEED SCRIPT
