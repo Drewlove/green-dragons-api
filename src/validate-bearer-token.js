eced48223ecb5ee24068e7ac5e709917c7e3c890
Drew Love <davidandrewlove84@gmail.com>
2021-05-04 17:10:11 -0400

    Add logging to validateBearerToken function

--- src/validate-bearer-token.js
+++ src/validate-bearer-token.js
@@ -3,10 +3,12 @@ const logger = require("./logger");
 function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get("Authorization");
-  console.log("API Token", apiToken);
-  console.log("authToken", authToken);
-  logger.error(apiToken, authToken);
-  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+  console.log(apiToken);
+  console.log(authToken);
+  logger.info(apiToken);
+  logger.info(authToken);
+  //authToken.split(" ")[1]
+  if (!authToken || authToken !== apiToken) {
     logger.error(`Unauthorized request to path: ${req.path}`);
     return res.status(401).json({ error: "unathorized request" });
   }
