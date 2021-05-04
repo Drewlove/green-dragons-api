fe0530f1d0f12a3a6e3e76386ed7ebe31156ff8d
Drew Love <davidandrewlove84@gmail.com>
2021-05-04 17:33:53 -0400

    Add ssl: { rejectUnauthorized: false } to server.js to resolve heroku error

--- src/server.js
+++ src/server.js
@@ -1,14 +1,28 @@
-const knex = require('knex')
-const app = require('./app')
-const {PORT, DATABASE_URL} = require('./config')
+const knex = require("knex");
+const app = require("./app");
+const { PORT, DATABASE_URL } = require("./config");
 
-const db = knex({
-    client: 'pg', 
-    connection: DATABASE_URL
-})
+if (NODE_ENV === "production") {
+  db = knex({
+    client: "pg",
+    connection: {
+      connectionString: DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
+    useNullAsDefault: true,
+  });
+} else {
+  db = knex({
+    client: "pg",
+    connection: {
+      connectionString: DATABASE_URL,
+    },
+    useNullAsDefault: true,
+  });
+}
 
-app.set('db', db)
+app.set("db", db);
 
 app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`)
-})
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
