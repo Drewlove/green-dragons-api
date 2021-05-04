d2a06e6da0408bfe3dd6f6c8a7fe15137387f628
Drew Love <davidandrewlove84@gmail.com>
2021-05-04 17:38:29 -0400

    Remove console.logs used for debugginer server error

--- src/validate-bearer-token.js
+++ src/validate-bearer-token.js
@@ -3,9 +3,6 @@ const logger = require("./logger");
 function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get("Authorization");
-  console.log(apiToken);
-  console.log(authToken);
-  //authToken.split(" ")[1]
   if (!authToken || authToken !== apiToken) {
     logger.error(`Unauthorized request to path: ${req.path}`);
     return res.status(401).json({ error: "unathorized request" });
